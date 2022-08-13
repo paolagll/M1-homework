db4d0a3a5e33c2c48d40b0d6ba250effc0cd5fe5
Paola Guerra <paola@192.168.1.10>
2022-08-12 22:21:52 -0300

    Done

--- 03-JavaScriptAvanzado-II/homework/homework.js
+++ 03-JavaScriptAvanzado-II/homework/homework.js
@@ -51,7 +51,7 @@ function cacheFunction(cb) {
    if (!cache.hasOwnProperty(arg)) {
     cache[arg] = cb(arg);
    }
-  return cb(arg);
+  return cache[arg];
  }
 }
 
@@ -79,8 +79,8 @@ function getNombre() {
   Usando el método bind() guardar, en las dos variables declaradas a continuación, dos funciones que actúen como getNombre pero retornen el nombre del instructor y del alumno, respectivamente.
 */
 
-let getNombreInstructor;
-let getNombreAlumno;
+let getNombreInstructor = getNombre.bind(instructor);
+let getNombreAlumno  = getNombre.bind(alumno);
 
 /*
   Ejercicio 4
