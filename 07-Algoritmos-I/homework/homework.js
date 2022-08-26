8cd06b261eb11593a3fc06b389da6cae9775fc4c
Paola Guerra <paola@192.168.1.9>
2022-08-25 22:17:17 -0300

    Done + Notas

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -17,14 +17,26 @@ function factorear(num) {
 //     div++;
 //     return factorear(num);
 // }
-var arrayfactor = [1];
-  for (let i = 2; i <= num; i++){
-    while(num%i === 0){
-      arrayfactor.push(i);
-      num=num/i;
-    };
-  };
-  return arrayfactor
+
+// var arrayfactor = [1];
+//   for (let i = 2; i <= num; i++){
+//     while(num%i === 0){
+//       arrayfactor.push(i);
+//       num=num/i;
+//     };
+//   };
+//   return arrayfactor;
+
+  let arr2 = [1];
+  let val = 2;
+  while (num > 1) {
+    if (num % val=== 0) {
+      arr2.push(val);
+      num = num/val;
+    } else {
+      val++
+    }
+  } return arr2
 }
 
 function bubbleSort(array) {
@@ -32,14 +44,30 @@ function bubbleSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-  for (let j = 0 ; j < array.length; j++){
-    for (let i = 0 ; i < array.length-j; i++){
-      if (array[i] > array[i+1]){
-        [array[i],array[i+1]] = [array[i+1],array[i]];
-        };
-    };
-  };
-  return array
+  //
+  // for (let j = 0 ; j < array.length; j++){
+  //   for (let i = 0 ; i < array.length-j; i++){
+  //     if (array[i] > array[i+1]){
+  //       [array[i],array[i+1]] = [array[i+1],array[i]];
+  //       };
+  //   };
+  // };
+  // return array;
+  //
+  let swap = true;
+
+  while (swap) {
+    swap = false;
+    for (let i = 0; i < array.length; i++) {
+      if(array[i] > array[i+1]){
+        let temp = array[i];
+        array[i] = array [i+1];
+        array[i+1] = temp;
+        swap = true;
+      }
+    }
+  }
+  return array;
 }
 
 
@@ -68,13 +96,28 @@ function selectionSort(array,arr2 = []) {
   // Devolver el array ordenado resultante
   // Tu código:
   //let arr2 =[]
-  for (let i = 1; i < array.length; i++) {
-    let curr = array[i];
-    for (var j = i - 1; j >= 0 && array[j] > curr; j--) {
-      array[j + 1] = array[j];
+  // for (let i = 1; i < array.length; i++) {
+  //   let curr = array[i];
+  //   for (var j = i - 1; j >= 0 && array[j] > curr; j--) {
+  //     array[j + 1] = array[j];
+  //   }
+  //   array[j + 1] = curr;
+  // }
+  // return array;
+  //
+  for (let i = 0; i < array.length; i++) {
+    let min = i;
+    for (let j = i+1; j < array.length; j++) {
+      if (array[min] > array[j]){
+        min = j
+      }
     }
-    array[j + 1] = curr;
+    if (i !== min) {
+    let temp = array [i];
+    array[i] = array[min];
+    array[min] = temp;
   }
+}
   return array;
 }
 
