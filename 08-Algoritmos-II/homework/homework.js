f157e2d376faaeb86a35bad627a961b87616e2ce
Paola Guerra <paola@192.168.1.9>
2022-08-30 23:23:45 -0300

    done

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -1,28 +1,43 @@
 'use strict'
 // No cambies los nombres de las funciones.
 
-function quickSort(array,) {
+function quickSort(array) {
   // Implementar el método conocido como quickSort para ordenar de menor a mayor
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  //
+  // let numRan = Math.random()*(array.length-1);
+  // let left =[];
+  // let right = []
+  // let eq = array[numRan]
+  // for (let i = 0; i < array.length; i++) {
+  //   if(array[i] < array[numRan]){
+  //     left.push(array[i]);
+  //   } else if (array[i] > array[numRan]){
+  //     right.push(array[i]);
+  //   } else if (array[i] === array[numRan]){
+  //    ;
+  //   }
+  // } 
+  if (array.length <= 1) return array; //la condicion para corte de recursion.
 
-  let numRan = Math.random()*(array.length-1);
+  let pivot = array[Math.floor(Math.random() * array.lenght)];
+
+  let left = [];
+  let right = [];
+  let equal = [];
   for (let i = 0; i < array.length; i++) {
-    if(array[i] < array[numRan]){
-      let izq = [];
-      izq.push(array[i]);
-      izq = quickSort(izq);
-    } else if (array[i] > array[numRan]){
-      let der =[];
-      der.push(array[i]);
-      der = quickSort(der);
-    } else if (array[i] === array[numRan]){
-      let eq = array[i];
+    if(array[i] < pivot){
+      left.push(array[i]);
+    } else if(array[i] > pivot){
+      right.push(array[i]);
+    } else {
+      equal.push(array[i]);
     }
     
-  } 
-  return 
+  }
+  return quickSort(left).concat(equal).concat(quickSort(right));
 }
 
 function mergeSort(array) {
@@ -30,8 +45,36 @@ function mergeSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  if (array.lenght === 1) return array;
 
+  let division = split(array);
+  let left = division[0];
+  let right = division[1];
 
+  return paste(mergeSort(left), mergeSort(right))
+}
+function split(array){
+  let middle = Math.floor(array.length / 2);
+  let left = array.slice(0,middle);
+  let right = array.slice(middle);
+  return [left,right];
+}
+
+function paste(left,right){
+  let array = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]){
+      array.push(left[leftIndex]);
+      leftIndex++; 
+    } else {
+      array.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+  return array.concat(left.slice(leftIndex)).concat(right.slice(rightIndex))
 }
 
 // No modificar nada debajo de esta línea
